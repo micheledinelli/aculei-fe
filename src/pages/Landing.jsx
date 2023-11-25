c943599c5c25a11ffc76bbdff69d8decd5ae1393
mussida <96059227+mussida@users.noreply.github.com>
2023-11-25 20:14:17 +0100

    fix: refactor title responsiveness

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -13,8 +13,10 @@ export default function Landing() {
   return (
     <div className="h-screen w-full overflow-x-hidden">
       <img className="object-cover w-full h-full" src={MainImage}></img>
-      <div className="absolute top-0 left-0 w-full h-2/5 flex flex-row items-center justify-center">
-        <div className="text-center w-full text-xxl text-white">{title}</div>
+      <div className=" absolute top-0 left-0 w-screen h-2/5 flex flex-row items-center justify-center">
+        <div className="text-center w-full overflow-x-hidden text-[8rem] sm:text-[12rem] md:text-[18rem] lg:text:text-[18rem] xl:text-[23rem]  text-white">
+          {title}
+        </div>
       </div>
       <div className="inline-block absolute w-full bottom-2 inset-x-0 text-white text-3xl text-center">
         <div className="flex flex-row justify-center items-center">
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontSize: {
-        xxl: "32em",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
