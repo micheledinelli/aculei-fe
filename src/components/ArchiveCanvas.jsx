078b69c2da0f9ef7232b6c9fc00747dc80eec95a
micheledinelli <dinellimichele00@gmail.com>
2023-12-01 13:11:59 +0100

    feat: added cam filter management by passing cam url param in the request

--- src/components/ArchiveCanvas.jsx
+++ src/components/ArchiveCanvas.jsx
@@ -25,12 +25,16 @@ const ArchiveCanvas = () => {
     };
   }, [fullScreenIndex]);
 
-  const fetchNewImage = async () => {
+  const fetchNewImage = async (cam) => {
     setIsLoading(true);
     try {
-      const response = await fetch(
-        import.meta.env.VITE_SERVER_URL + "/api/v1/image"
-      );
+      let apiUrl = import.meta.env.VITE_SERVER_URL + "/api/v1/image";
+      if (cam !== null && cam !== undefined) {
+        apiUrl += `?cam=${cam}`;
+      }
+
+      const response = await fetch(apiUrl);
+
       if (response.ok) {
         const imageUrl = URL.createObjectURL(await response.blob());
         const newImage = {
@@ -57,15 +61,14 @@ const ArchiveCanvas = () => {
     }
   };
 
-  const spawnRandomImage = () => {
-    fetchNewImage();
+  const spawnImage = (cam) => {
+    fetchNewImage(cam);
     if (isInGridMode) {
       toggleGridMode();
     }
   };
 
   const toggleFullScreen = (index) => {
-    console.log("index", index);
     setImages((prevImages) => {
       const updatedImages = [...prevImages];
       updatedImages[index].isFullScreen = !updatedImages[index].isFullScreen;
@@ -275,43 +278,34 @@ const ArchiveCanvas = () => {
           </div>
         ))}
       </div> */}
-      {!isInGridMode && !isLoading ? (
+      {!isInGridMode ? (
         <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 text-center rounded-lg bg-gray-500 bg-opacity-50 flex text-2xl p-3 z-10">
-          <button
-            className="mx-2 hover:scale-110"
-            onClick={() => spawnRandomImage()}
-          >
+          <button className="mx-2 hover:scale-110" onClick={() => spawnImage()}>
             ANIMAL
           </button>
-          <button
-            className="mx-2 hover:scale-110"
-            onClick={() => spawnRandomImage()}
-          >
+          <button className="mx-2 hover:scale-110" onClick={() => spawnImage()}>
             MOON
           </button>
-          <button
-            className="mx-2 hover:scale-110"
-            onClick={() => spawnRandomImage()}
-          >
+          <button className="mx-2 hover:scale-110" onClick={() => spawnImage()}>
             C° or F°
           </button>
-          <button
-            className="mx-2 hover:scale-110"
-            onClick={() => spawnRandomImage()}
-          >
+          {/* <button className="mx-2 hover:scale-110" onClick={() => spawnImage()}>
             CAM
-          </button>
-        </div>
-      ) : isLoading ? (
-        <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 p-3 z-10 text-white">
-          <svg
-            viewBox="0 0 1024 1024"
-            fill="currentcolor"
-            stroke="currentcolor"
-            className="w-10 h-10 animate-spin"
+          </button> */}
+          <select
+            id="cams"
+            className="mx-2 hover:scale-110 bg-inherit p-1 rounded-md text-center cursor-pointer"
+            onChange={(e) => spawnImage(e.target.value)}
+            onDoubleClick={(e) => spawnImage(e.target.value)}
           >
-            <path d="M512 1024c-69.1 0-136.2-13.5-199.3-40.2C251.7 958 197 921 150 874c-47-47-84-101.7-109.8-162.7C13.5 648.2 0 581.1 0 512c0-19.9 16.1-36 36-36s36 16.1 36 36c0 59.4 11.6 117 34.6 171.3 22.2 52.4 53.9 99.5 94.3 139.9 40.4 40.4 87.5 72.2 139.9 94.3C395 940.4 452.6 952 512 952c59.4 0 117-11.6 171.3-34.6 52.4-22.2 99.5-53.9 139.9-94.3 40.4-40.4 72.2-87.5 94.3-139.9C940.4 629 952 571.4 952 512c0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 00-94.3-139.9 437.71 437.71 0 00-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.2C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7 26.7 63.1 40.2 130.2 40.2 199.3s-13.5 136.2-40.2 199.3C958 772.3 921 827 874 874c-47 47-101.8 83.9-162.7 109.7-63.1 26.8-130.2 40.3-199.3 40.3z" />
-          </svg>
+            <option value="1">CAM1</option>
+            <option value="2">CAM2</option>
+            <option value="3">CAM3</option>
+            <option value="4">CAM4</option>
+            <option value="5">CAM5</option>
+            <option value="6">CAM6</option>
+            <option value="7">CAM7</option>
+          </select>
         </div>
       ) : (
         <></>
