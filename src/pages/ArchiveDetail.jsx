111c6a5a07f8cb8f013aa6aba1147be1b1a1ef23
mussida <96059227+mussida@users.noreply.github.com>
2024-01-19 13:05:46 +0100

    fix: add scroll to top after image from cluster selected

--- src/pages/ArchiveDetail.jsx
+++ src/pages/ArchiveDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useScramble } from "use-scramble";
 import { useNavigate, useParams } from "react-router-dom";
@@ -26,6 +26,12 @@ export default function ArchiveDetail() {
     scramble: 4,
     seed: 0,
   });
+  const location = useLocation();
+
+  useEffect(() => {
+    setImages([]);
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
 
   const handleImageClick = (image) => {
     setImages([]);
