14262fe135704701ab6ad53a7740e826cad823e3
micheledinelli <dinellimichele00@gmail.com>
2024-01-21 10:14:48 +0100

    feat: modified loading gif

--- src/components/LoadingGif.jsx
+++ src/components/LoadingGif.jsx
@@ -10,19 +10,19 @@ function LoadingGif() {
   // Randomly select a gif path
   const randomGif = gifPaths[Math.floor(Math.random() * gifPaths.length)];
 
-  const shouldInvert = Math.random() < 0.5;
-  const invertClass = shouldInvert ? "invert" : "";
+  // const shouldInvert = Math.random() < 0.5;
+  // const invertClass = shouldInvert ? "invert" : "";
 
   return (
     <div
-      className={`fixed top-0 left-0 h-full w-full overflow-hidden flex items-center justify-center z-50 contrast-125 hue-rotate-15 ${invertClass}`}
+      className={`fixed top-0 left-0 h-full w-full overflow-hidden flex items-center justify-center z-50`}
     >
       <img
         src={randomGif}
         className="w-full h-full object-cover absolute"
         alt="Loading"
       />
-      <div className="font-noto text-5xl absolute z-50">
+      <div className="font-noto text-8xl absolute z-50 text-white">
         <ScrambleText text="Loading" keepmovin={true} />
       </div>
     </div>
