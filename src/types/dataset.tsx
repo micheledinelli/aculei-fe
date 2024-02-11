1a855e05f70490579779dff2ab4ea13e06d369d3
micheledinelli <dinellimichele00@gmail.com>
2024-02-11 17:55:37 +0100

    feat: added data in info dataset

--- src/types/dataset.tsx
+++ src/types/dataset.tsx
@@ -1,29 +1,3 @@
-interface AnimalCounts {
-  [animal: string]: number;
-}
-
-interface CameraCounts {
-  [camera: string]: number;
-}
-
-interface MoonPhaseCounts {
-  [phase: string]: number;
-}
-
-interface SeasonCounts {
-  [season: string]: number;
-}
-
-interface Temperature {
-  max: number;
-  min: number;
-}
-
-interface DatasetInfo {
-  animals: AnimalCounts;
-  cameras: CameraCounts;
-  moon_phases: MoonPhaseCounts;
-  seasons: SeasonCounts;
-  temperature: Temperature;
-  total_records: number;
-}
+type DatasetInfo = {
+  [key: string]: string | number;
+};
