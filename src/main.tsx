bf128c7249c28dd60d75d1307829075fd0d0fcaa
micheledinelli <dinellimichele00@gmail.com>
2024-02-13 19:27:58 +0100

    feat: added mobile message

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,9 @@ import Landing from "./pages/Landing";
 import Experience from "./pages/Experience";
 import Error from "./pages/Error";
 import { ExperienceProvider } from "./contexts/ExperienceContext";
+import Mobile from "./pages/Mobile";
+
+const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
 const router = createBrowserRouter([
   {
@@ -20,6 +23,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ExperienceProvider>
-    <RouterProvider router={router} />
+    {isMobile ? <Mobile /> : <RouterProvider router={router} />}
   </ExperienceProvider>
 );
